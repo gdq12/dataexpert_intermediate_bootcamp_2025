87d4b0c0ed9491735056b8a67885eb13c0bf68c3
gdq12 <gdquiceno@gmail.com>
2026-04-05 09:58:57 +0200

    cleaned up queries for snowflake setup a bit

--- setup_dbt/snowflake_queries.sql
+++ setup_dbt/snowflake_queries.sql
@@ -1,46 +1,40 @@
--- queries for dbt-snowflake connex and dbt project setup 
+-- create needed sources 
 create role dbt_role;
 
-create user DBT_USER password='DBT_PWD';
+create role data_loader;
+
+create user DBT_USER password = 'DBT_PWD';
+
+grant role dbt_role to user DBT_USER;
 
 create warehouse DBT_WAREHOUSE
-warehouse_size='X-SMALL'
+warehouse_size = 'X-SMALL'
 ;
 
+-- where dbt model data will be sourced/created/maintained
 create database dataexpert;
 
 create schema dataexpert.dbt_basic;
 
+-- needed permission to load source data 
+grant usage on database dataexpert to role data_loader;
+
+grant usage on schema dataexpert.dbt_basic to role data_loader;
+
+grant create table on schema dataexpert.dbt_basic to role data_loader;
+
+-- needed permissions for project run
 grant usage on database dataexpert to role dbt_role;
 
 grant usage on schema dataexpert.dbt_basic to role dbt_role;
-
-grant role dbt_role to user DBT_USER;
 
 grant create table on schema dataexpert.dbt_basic to role dbt_role;
 
 grant create view on schema dataexpert.dbt_basic to role dbt_role;
 
-alter user dbt_user set default_warehouse='DBT_WAREHOUSE';
-
 grant usage on warehouse dbt_warehouse to role dbt_role;
 
 grant select on all tables in schema dataexpert.dbt_basic to role dbt_role;
-
-grant select on future tables in schema dataexpert.dbt_basic to role dbt_role;
-
--- queries to setup source data not used in the end
-
--- create role and grant permissions  
-create role data_loader;
-
-grant usage on database dataexpert to role data_loader;
-
-grant usage on schema dataexpert.dbt_basic to role data_loader;
-
-grant create stage on schema dataexpert.dbt_basic to role data_loader;
-
-grant create table on schema dataexpert.dbt_basic to role data_loader;
 
 -- for snowsight UI work
 grant role data_loader to user gdq12;
@@ -48,6 +42,11 @@ grant role data_loader to user gdq12;
 grant role dbt_role to user gdq12;
 
 -- -- eventually not needed
+
+-- grant create stage on schema dataexpert.dbt_basic to role data_loader;
+-- alter user dbt_user set default_warehouse='DBT_WAREHOUSE';
+-- grant select on future tables in schema dataexpert.dbt_basic to role dbt_role;
+
 -- -- stage for loading data 
 -- CREATE STAGE my_s3_public_stage
 --   URL = 's3://dbt-tutorial-public/long_term_dataset/'
